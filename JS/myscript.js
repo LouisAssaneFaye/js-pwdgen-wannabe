045dd9fc981b72c99222d6277b007cf970bd2b06
LouisAssaneFaye <louis.faye.18@outlook.it>
2023-05-11 19:00:03 +0200

    Esercizio terminato

---
+++ JS/myscript.js
@@ -0,0 +1,6 @@
+const nome=prompt("Type your name");
+const cognome=prompt("Type your surname");
+const colore=prompt("Type your color");
+const somma=nome + cognome + colore 
+
+document.getElementById("paragrafo").innerHTML=somma + 21;
